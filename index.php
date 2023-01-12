b61067444442fdfeef6bd63eeebc321cf96d278a
Damien <damien.deboudt@sfr.fr>
2023-01-12 13:22:53 +0100

    ok

--- index.php
+++ index.php
@@ -4,22 +4,22 @@
  * 1. Affichez dans un <p> la valeur absolue de -8
  */
 $value = -8;
+echo "<p>" . abs($value);
 // TODO Votre code ici.
-
 
 /**
  * 2. Affichez dans un <p> la valeur de 5.64 arrondit au nombre supérieur.
  */
 $value = 5.64;
 // TODO Votre code ici.
-
+echo "<p>" . ceil($value);
 
 /**
  * 3. Affichez dans un <p> la valeur de 5.34 arrondit au nombre inférieur.
  */
 $value = 5.34;
 // TODO Votre code ici.
-
+echo "<p>" . floor($value);
 
 /**
  * 4. Affichez dans un <p> la valeur maximum du tableau $max, et la valeur minimum du tableau $min.
@@ -27,14 +27,15 @@ $value = 5.34;
 $max = [5, 10, 12, 8, 7, 6, 54, 39, 76, 63, 84, 2, 21];
 $min = [5, 41, 65, 4, 3, 65, 2, 65, 74, 51, 23, 39, 42];
 // TODO Votre code ici.
-
+echo "<p>" . max($max);
+echo "<p>" . min($min);
 
 /**
  * 5. Affichez dans un <p> la valeur de PI * 2 ( avec une fonction mathématique pour PI ).
  */
 
 // TODO Votre code ici.
-
+echo "<p>" .pi() * 2;
 
 /**
  * 6. Affichez dans un <p> la valeur arrondie de 5.42 avec la fonction round().
@@ -43,6 +44,6 @@ $value = 5.42;
 // TODO Votre code ici.
 
 
-
+echo "<p>" . round($value);
 
 
